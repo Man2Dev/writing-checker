3e3b0ce369c13fdf73e18857b4b8de1ad8b4d190
Man2Dev <hendiani.mohammadreza@gmail.com>
2022-02-13 09:27:50 -0800

    main

--- Branch.py
+++ Branch.py
@@ -3,21 +3,39 @@ import language_tool_python
 tool = language_tool_python.LanguageTool("en-US")
 
 # input text
-text = "A sentence with a error in the Hitchhiker’s Guide tot he Galaxy. hallo how are yoo."
+text = "a santence with a error in the Hitchhiker’s Guide tot he Galaxy. hallo how are yoo."
 
 matches = tool.check(text)
+
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
 # printing all mistakes
 i = 0
 for match in matches:
+    print(f"\nMatch {i+1}:")
+    print("1\nMessage:\n", match.message, "\n")
+    print("2\nOffset:", matches[i].offsetInContext, "\n")
+    print("3\nerrorLength:", matches[i].errorLength, "\n")
+    print("4\ncontext:", matches[i].context, "\n")
+    print("5\nCategory:", matches[i].category, "\n")
+    print("6\nRule Issue Type:", matches[i].ruleIssueType, "\n")
+    print("7\nSentence:", matches[i].sentence, "\n")
+    print("A\nID [Suggestion]:\n", matches[i].ruleId, matches[i].replacements, "\n")
+    print("B\nMatche:\n", matches[i])
+    print("-----------------------------------------------------------------")
     i += 1
-    print(f"\nMatch {i}:")
-    print(match.message)
 
-print("\n")
+print("\n##############################################################################################################")
 
 print("Number of mistakes:\t", len(matches))
 print("Input text:\t\t", text)
-print("Auto Correted:\t\t", language_tool_python.utils.correct(text, matches), "\n")
+print("Auto Correted:\t\t", tool.correct(text))
+print("Auto Correted:\t\t", language_tool_python.utils.correct(text, matches))
+
+print("##############################################################################################################")
+
+re = get_match_ordered_dict()
+print(re)
+
 
 tool.close()  # Call `close()` to shut off the server when you're done.
