b7288e86893910ef041febef61af70c14e79100c
Man2Dev <hendiani.mohammadreza@gmail.com>
2022-02-18 07:33:03 -0800

    DB TODO

--- Main.py
+++ Main.py
@@ -12,14 +12,22 @@ print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 # printing all mistakes
 i = 0
 for match in matches:
-    print(f"\nMatch {i+1}:\n")
+    #print(f"\nMatch {i+1}:\n")
+    # err text
     print("1\nMessage:\n", match.message, "\n")
+    # errOffset
     print("2\nOffset:", matches[i].offsetInContext, "\n")
+    # errLength
     print("3\nerrorLength:", matches[i].errorLength, "\n")
+    # context
     print("4\ncontext:", matches[i].context, "\n")
+    # category
     print("5\nCategory:", matches[i].category, "\n")
+    # ruleIssue
     print("6\nRule Issue Type:", matches[i].ruleIssueType, "\n")
+    # entence
     print("7\nSentence:", matches[i].sentence, "\n")
+    # numberSuggestion, suggestion
     print(
         f"8\nID [{len(matches[i].replacements)} Suggestions]:\n",
         matches[i].ruleId,
--- mistakesDB.py
+++ mistakesDB.py
@@ -10,3 +10,9 @@ c.execute("""CREATE TABLE data (
             last text,
             pay integer
             )""")
+
+def new_user_operator(chat_id, first_name, username, phone_number,is_operator):
+    cursor.execute("""INSERT INTO operator (chat_id, first_name, username, phone_number, is_making_order, is_operator,
+     is_administrator) VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (chat_id, first_name, username, phone_number, 0, 1, 0))
+    conn.commit()
