3e7868f5151a9a29f71f111ffd4c4849df4343a1
erfanghorbanee <erfanghorbanee@gmail.com>
2022-02-22 09:59:18 +0330

    database.py updated

--- database.py
+++ database.py
@@ -1,102 +1,32 @@
 import sqlite3
 
-# con = sqlite3.connect("database.db")
-conn.execute("PRAGMA foreign_keys = 1")
-# cursor = con.cursor()
-
-# cursor.executescript(
-#     """CREATE TABLE IF NOT EXISTS exams
-#     (
-#         ExamID INTEGER not null
-#             primary key autoincrement,
-#         image BLOB,
-#         MistakesNumber INTEGER,
-#         inputText VARCHAR,
-#         CorrectAnswer VARCHAR,
-#         Created INTEGER,
-#         finished INTEGER
-#     );
-#     """
-# )
-
-# cursor.executescript(
-#     """CREATE TABLE IF NOT EXISTS match
-#     (
-#         id INTEGER not null
-#             primary key autoincrement,
-#         ErrorText VARCHAR,
-#         ErrorOffset INTEGER,
-#         ErrorLength INTEGER
-#         Context VARCHAR,
-#         Category VARCHAR,
-#         ruleIssue VARCHAR,
-#         sentence VARCHAR,
-#         ruleID VARCHAR,
-#         numberSuggestions INTEGER,
-#         Suggestions VARCHAR,
-#         FOREIGN KEY(ExamID) REFERENCES exams(ExamID))
-#     );
-#     """
-# )
-
-# # def add_exam(
-# #     ErrorText, ErrorOffset, ErrorLength, image,
-# #     Context, Category, ruleIssue, sentence,
-# #     ruleID, numberSuggestions, Suggestions, MistakesNumber,
-# #     inputText, CorrectAnswer, Created, finished,
-# # ):
-
-# #     cursor.execute(
-# #         """INSERT INTO exams (ErrorText, ErrorOffset, ErrorLength, image, Context,
-# #         Category, ruleIssue, sentence, ruleID, numberSuggestions, Suggestions,
-# #         MistakesNumber, inputText, CorrectAnswer, Created, finished) 
-# #         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-# #         (
-# #             ErrorText,
-# #             ErrorOffset,
-# #             ErrorLength,
-# #             image,
-# #             Context,
-# #             Category,
-# #             ruleIssue,
-# #             sentence,
-# #             ruleID,
-# #             numberSuggestions,
-# #             Suggestions,
-# #             MistakesNumber,
-# #             inputText,
-# #             CorrectAnswer,
-# #             Created,
-# #             finished,
-# #         ),
-# #     )
-# #     con.commit()
-
-
-
-conn=sqlite3.connect("clientdatabase.db")
+conn=sqlite3.connect("database.db")
 conn.execute("PRAGMA foreign_keys = 1")
 cur=conn.cursor()
 
-# Create 2 tables if they don't exist: Clients and Work_Done
-cur.execute('''CREATE TABLE IF NOT EXISTS Clients
-(CID INTEGER PRIMARY KEY,
-First_Name  TEXT    NOT NULL,
-Last_Name       TEXT,
-Business_Name   TEXT,
-Phone           TEXT,
-Address         TEXT,
-City            TEXT,
-Notes           TEXT,
-Active_Status   TEXT    NOT NULL)''')      
+# Create 2 tables if they don't exist: Exams and Match
+cur.execute('''CREATE TABLE IF NOT EXISTS Exams
+(EID INTEGER PRIMARY KEY,
+image BLOB,
+MistakesNumber INTEGER,
+inputText VARCHAR,
+CorrectAnswer VARCHAR,
+Created TEXT,
+finished TEXT)''')      
 
-cur.execute('''CREATE TABLE IF NOT EXISTS Work_Done
+cur.execute('''CREATE TABLE IF NOT EXISTS Match
 (ID INTEGER PRIMARY KEY,
-Date            TEXT    NOT NULL,
-Onsite_Contact  TEXT,
-Work_Done       TEXT    NOT NULL,
-Parts_Installed TEXT,
-Next_Steps      TEXT,
-CID             INT,
-FOREIGN KEY (CID) REFERENCES CLIENTS (CID))''')
+ErrorText VARCHAR,
+ErrorOffset INTEGER,
+ErrorLength INTEGER
+Context VARCHAR,
+Category VARCHAR,
+ruleIssue VARCHAR,
+sentence VARCHAR,
+ruleID VARCHAR,
+numberSuggestions INTEGER,
+Suggestions VARCHAR,
+EID             INT,
+FOREIGN KEY (EID) REFERENCES Exams (EID))''')
+
 conn.commit()
