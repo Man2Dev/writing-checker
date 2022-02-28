cc12a5060729c9a0498657ae6f75c9a1ba56436a
erfanghorbanee <erfanghorbanee@gmail.com>
2022-02-28 22:08:04 +0330

    main ouput will be saved in the database

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ MistakesNumber INTEGER,
 user_answer VARCHAR,
 CorrectAnswer VARCHAR,
 Created DATETIME DEFAULT CURRENT_TIMESTAMP,
-finished DATETIME)"""
+finished DATETIME DEFAULT NULL)"""
 )
 
 cur.execute(
@@ -36,13 +36,16 @@ FOREIGN KEY (EID) REFERENCES Exams (EID))"""
 
 conn.commit()
 
-# create an instance of Exams table
+
+# create an instance of Exams table and return its id
 def insert_exam(entities):
     cur.execute(
-        "INSERT INTO Exams(question_name, question, MistakesNumber, user_answer, CorrectAnswer, Created, finished) VALUES(?, ?, ?, ?, ?, ?, ?)",
+        "INSERT INTO Exams(question_name, question, MistakesNumber, user_answer, CorrectAnswer, Created) VALUES(?, ?, ?, ?, ?, ?)",
         entities,
     )
     conn.commit()
+
+    return cur.lastrowid
 
 
 # return a list of user's exams
@@ -85,22 +88,37 @@ def fetch_matches(fk):
 
 
 # example...
-exam = (
-    "first question",
-    "what is your name child?",
-    5,
-    "I'm hendiani! the guz master!",
-    "there is no correct answer!",
-    1400,
-    1400,
-)
-insert_exam(exam)
+# exam = (
+#     "first question",
+#     "what is your name child?",
+#     5,
+#     "I'm hendiani! the guz master!",
+#     "there is no correct answer!",
+#     1400,
+#     1400,
+# )
+# insert_exam(exam)
 
-match = ("error text", 3, 4, "dddd", "aa", "ss", "dd", "dd", 3, "sd", 1)
-insert_match(match)
+# match = ("error text", 3, 4, "dddd", "aa", "ss", "dd", "dd", 3, "sd", 1)
+# insert_match(match)
 
 # delete_match(1)
 
-print(fetch_exams())
-print("\n")
-print(fetch_matches(1))
+# print(fetch_exams())
+# print("\n")
+# print(fetch_matches(1))
+
+answer = """The world today uses more renewable energy than ever before since it contributes to the preservation of the environment and is economically sound; however, some argue that green energy could undermine the reliability of the global supply as a result of its dependency on climatic and meteorological phenomena. This essay will examine both views, but personally, I strongly advocate the adoption of renewable sources of energy.
+
+On the one hand, opponents of renewables claim that the world could face disruptions to the power supply should they be fully adopted. This is largely due to the fact that many green energy technologies currently in use depend on changeable and unpredictable phenomena such as wind, rain and cloud cover.
+
+To take the fastest growing sector as an example, solar panels can only be used in the presence of strong and direct sunlight, and although the problem of directness has already been somewhat solved with moving panel arrays, a cloudy few days could still result in a blackout if we depended entirely on solar power; something that is unlikely to occur today given current oil stockpiles.
+
+However, fossil fuels are a larger threat to energy security since they are certainly finite in quantity, whereas renewable energy is effectively infinite; once the Earth’s oil is depleted, there will be no energy security without green energy technologies.
+
+Furthermore, problems of unpredictability can be mitigated by improving battery technologies (to create a larger buffer), building more renewable energy generators (to increase supply during ideal conditions) and improving current technologies (to increase efficiency), such as in the moving solar panel example. Proponents of renewables therefore claim that they are the most economically sound option.
+
+To conclude, while many may believe that green energy technologies are a threat to energy security, the fossil fuels they frequently promote are a greater threat, and renewables are in fact the only sound option, both economically and environmentally."""
+
+
+question = """Nowadays most green energy is becoming evermore prevalent in both developed and developing countries. Some argue they greatly reduce costs and are better for the environment, others believe they are a serious threat to energy security. Discuss both views and give your opinion."""
--- main.py
+++ main.py
@@ -1,55 +1,46 @@
+from datetime import datetime
+
 import language_tool_python
 
-# from database import insert_exam
+from database import answer, insert_exam, insert_match, question
 
 tool = language_tool_python.LanguageTool("en-US")
 
-# input text
-text = "a santence with a error in the Hitchhiker’s Guide tot he Galaxy. hallo how are yoo."
+matches = tool.check(answer)
+miatakes_number = len(matches)
+autoCorrected = tool.correct(answer)
+created = datetime.now()
 
-matches = tool.check(text)
+exam = ("Question 1", question, miatakes_number, answer, autoCorrected, created)
+exam_id = insert_exam(exam)
 
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-# printing all mistakes
-i = 0
 for match in matches:
-    # print(f"\nMatch {i+1}:\n")
-    # errText
-    print("1\nMessage:\n", match.message, "\n")
-    # errOffset
-    print("2\nOffset:", matches[i].offsetInContext, "\n")
-    # errLength
-    print("3\nerrorLength:", matches[i].errorLength, "\n")
-    # context
-    print("4\ncontext:", matches[i].context, "\n")
-    # category
-    print("5\nCategory:", matches[i].category, "\n")
-    # ruleIssue
-    print("6\nRule Issue Type:", matches[i].ruleIssueType, "\n")
-    # sentence
-    print("7\nSentence:", matches[i].sentence, "\n")
-    # numberSuggestion, suggestion
-    print(
-        f"8\nID [{len(matches[i].replacements)} Suggestions]:\n",
-        # ruleID
-        matches[i].ruleId,
-        matches[i].replacements,
-        "\n",
+    errText = match.message
+    errOffset = match.offsetInContext
+    errLength = match.errorLength
+    context = match.context
+    category = match.category
+    ruleIssue = match.ruleIssueType
+    sentence = match.sentence
+    numberSuggestions = len(match.replacements)
+    ruleID = match.ruleId
+    Suggestions = match.replacements
+
+    m = (
+        errText,
+        errOffset,
+        errLength,
+        context,
+        category,
+        ruleIssue,
+        sentence,
+        ruleID,
+        numberSuggestions,
+        str(Suggestions),
+        exam_id,
     )
-    # print("9\nMatche:\n", matches[i])
-    print("-" * 90)
-    i += 1
 
-print("\n" + 110 * "#")
-# numberMistakes
-print("Number of mistakes:\t", len(matches))
-# inputText
-print("Input text:\t\t", text)
-# autoCorrected
-print("Auto Corrected:\t\t", tool.correct(text))
-print("Auto Corrected:\t\t", language_tool_python.utils.correct(text, matches))
-
-print(110 * "#")
+    insert_match(m)
 
 tool.close()  # Call `close()` to shut off the server when you're done.
